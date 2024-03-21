5a77153a5293d9cf3fe3eba0eeadb12928afa356
Ly Thai Cuong <ltc1407.5532@gmail.com>
2024-03-21 14:25:49 +0700

    commit again

---
+++ .history/src/pages/ProductDetail/OrderProduct/index_20240321135443.js
@@ -0,0 +1,211 @@
+import { useState, useEffect, useContext } from "react"; // React hooks
+import { useParams } from "react-router-dom"; // React router
+import { Link, useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Container, Row, Col, Button } from "react-bootstrap"; // Bootstrap
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Font Awesome
+import {
+  faBox,
+  faCartPlus,
+  faCartShopping,
+  faHandHoldingDollar,
+  faStar,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons"; // Font Awesome
+
+import { AuthContext } from "~/functions/authContext";
+import { interactData } from "~/functions/interactData"; // Custom function
+import {
+  handleDecrease,
+  handleIncrease,
+  handleQuantityChange,
+  handleResponse,
+} from "~/functions/eventHandlers"; // Custom functions
+import { renderStarIcons } from "~/functions/render"; // Custom function
+import { LoadingAnimation, Title } from "~/components"; // Custom components
+import { addToCartURL, priceURL, productDetailURL } from "~/data";
+
+import classNames from "classnames/bind"; // CSS Module
+import styles from "./ProductDetail.module.scss"; // CSS Module
+const cx = classNames.bind(styles); // CSS Module
+
+function OrderProduct() {
+    const { isLoggedIn } = useContext(AuthContext);
+  const [userData, setUserData] = useState({
+    id: "",
+  });
+  const [productDetail, setProductDetail] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const [sizes, setSizes] = useState([]);
+  const [selectedSize, setSelectedSize] = useState("");
+  const [sizePrice, setSizePrice] = useState(0);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    interactData(`${productDetailURL}?productID=${id}`, "GET", null, (data) => {
+      setProductDetail(data.productDetail);
+      setSizes(data.sizes);
+    });
+  }, [id]);
+
+  useEffect(() => {
+    if (selectedSize) {
+      interactData(
+        `${priceURL}?productID=${id}&sizeName=${selectedSize}`,
+        "GET",
+        null,
+        setSizePrice
+      );
+    }
+  }, [id, selectedSize]);
+
+  const totalPrice = () => {
+    if (sizePrice) {
+      return parseFloat(sizePrice[0].Price * quantity).toFixed(2);
+    } else {
+      return parseFloat(productDetail.DefaultPrice * quantity).toFixed(2);
+    }
+  };
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData) {
+      setUserData(userData);
+    }
+  }, []);
+
+    return ( 
+        <div>
+            {/* Name */}
+            <Container className={cx("product-header")}>
+              <h2 className={cx("product-name")}>{productDetail.FigureName}</h2>
+            </Container>
+
+            {/* Metrics */}
+            <Container className={cx("product-metrics")}>
+              <Container className={cx("product-rating")}>
+                5.0{" "}
+                <span className={cx("metrics-data")}>
+                  {productDetail.AverageRating}
+                </span>
+                <FontAwesomeIcon className={cx("rating-icon")} icon={faStar} />
+                {starIcons}
+              </Container>
+              <Container className={cx("product-reviews")}>
+                <span className={cx("metrics-data")}>
+                  {productDetail.TotalReviews}
+                </span>{" "}
+                Evaluations
+              </Container>
+              <Container className={cx("product-sold")}>
+                <span className={cx("metrics-data")}>{productDetail.Sold}</span>{" "}
+                Sold
+              </Container>
+              <Container className={cx("product-available")}>
+                <span className={cx("metrics-data")}>
+                  {productDetail.TotalQuantity}
+                </span>{" "}
+                Available
+              </Container>
+            </Container>
+
+            {/* Price */}
+            <Container className={cx("product-price")}>
+              <span className={cx("price")}>${totalPrice()}</span>
+            </Container>
+
+            {/* Options */}
+            <Container className={cx("product-options")}>
+              {/* Size */}
+              <Container className={cx("product-size")}>
+                <span className={cx("option-label")}>Size:</span>
+                <Container className={cx("size-select")}>
+                  {sizes.map((size) => (
+                    <Button
+                      key={size.ID}
+                      className={cx("size-option", {
+                        "size-selected": size.SizeName === selectedSize,
+                      })}
+                      onClick={() => handleSizeSelection(size.SizeName)}
+                    >
+                      {size.SizeName}
+                    </Button>
+                  ))}
+                  {error && (
+                    <span className={cx("error-message")}>{error}</span>
+                  )}
+                </Container>
+              </Container>
+
+              {/* Quantity */}
+              <Container className={cx("product-quantity")}>
+                <span className={cx("option-label")}>Quantity:</span>
+                <Container className={cx("quantity-select")}>
+                  <Button
+                    className={cx("decrease")}
+                    onClick={() => handleDecrease(quantity, setQuantity)}
+                  >
+                    -
+                  </Button>
+                  <input
+                    type="text"
+                    value={quantity}
+                    onChange={(event) =>
+                      handleQuantityChange(event, setQuantity)
+                    }
+                    className={cx("quantity-input")}
+                  />
+                  <Button
+                    className={cx("increase")}
+                    onClick={() => handleIncrease(quantity, setQuantity)}
+                  >
+                    +
+                  </Button>
+                </Container>
+              </Container>
+            </Container>
+
+            {/* Action */}
+            <Container className={cx("product-action")}>
+              <Button className={cx("add-to-cart")} onClick={handleAddToCart}>
+                <FontAwesomeIcon icon={faCartPlus} />
+                <span className={cx("add-to-cart__text")}>Add to Cart</span>
+              </Button>
+              <Button className={cx("buy")}>
+                <FontAwesomeIcon icon={faCartShopping} />
+                <span className={cx("buy__text")}>Buy</span>
+              </Button>
+            </Container>
+
+            {/* Policy */}
+            <Container className={cx("product-policy")}>
+              <Row>
+                <Col className={cx("policy-container")} lg={4}>
+                  <FontAwesomeIcon icon={faHandHoldingDollar} />
+                  <span className={cx("policy-text")}>
+                    111% refund if the product is fake
+                  </span>
+                </Col>
+                <Col className={cx("policy-container")} lg={4}>
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                  <span className={cx("policy-text")}>
+                    Open the box to check and receive the goods
+                  </span>
+                </Col>
+                <Col className={cx("policy-container")} lg={4}>
+                  <FontAwesomeIcon icon={faBox} />
+                  <span className={cx("policy-text")}>
+                    Return for manufacturer's error
+                  </span>
+                </Col>
+              </Row>
+            </Container>
+        </div>
+     );
+}
+
+export default OrderProduct;
